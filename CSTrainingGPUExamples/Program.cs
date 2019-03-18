59d944a78d55d572839f7715a5dadbabc84c9b87
am zare <amin.zare@gmail.com>
2019-03-18 10:50:37 +0330

    init and commit all

---
+++ CSTrainingGPUExamples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNTK.CSTrainingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = DeviceDescriptor.GPUDevice(0);
+            Console.WriteLine($"======== running LogisticRegression.TrainAndEvaluate using {device.Type} ========");
+            LogisticRegression.TrainAndEvaluate(device);
+
+
+            Console.WriteLine($"======== running TransferLearning.TrainAndEvaluateWithAnimalData using {device.Type} ========");
+            TransferLearning.TrainAndEvaluateWithAnimalData(device, true);
+
+        }
+    }
+}
